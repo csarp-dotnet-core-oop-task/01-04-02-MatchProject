641da4f667fdc2ed90685126fc9d76216dd6db5f
Csaba Gyuris <gycsaba@vasvari.hu>
2022-02-17 13:29:34 +0100

    Feladat és teszt

---
+++ tests/TestsMatch.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+using MatchProject;
+
+namespace MatchProject.Tests
+{
+    [TestClass()]
+    public class TestsMatch
+    {
+        [TestMethod()]
+        public void TestConstructor()
+        {
+            try
+            {
+                Result firstMatch = new Result(2, 1);
+                Result secondMatch = new Result(2, 1);
+                Match match = new Match("FTC", "Szeged", firstMatch, secondMatch);
+            }
+            catch (Exception e)
+            {
+                Assert.Fail("A Match constructor hibát dob.");
+            }
+        }
+
+        [TestMethod()]
+        public void TestWinnerFirstPoint()
+        {
+            Result firstMatch = new Result(3, 1);
+            Result secondMatch = new Result(2, 1);
+            Match match = new Match("FTC", "Szeged", firstMatch, secondMatch);
+            string expected = "FTC";
+            string actaul = match.Winner;
+            Assert.AreEqual(expected, actaul, "A győztest nem jól határozta meg! Első csapat pontokkal!");
+        }
+        [TestMethod()]
+        public void TestWinnerSecondPoint()
+        {
+            Result firstMatch = new Result(3, 3);
+            Result secondMatch = new Result(2, 1);
+            Match match = new Match("FTC", "Szeged", firstMatch, secondMatch);
+            string expected = "Szeged";
+            string actaul = match.Winner;
+            Assert.AreEqual(expected, actaul, "A győztest nem jól határozta meg! Második csapat pontokkal!");
+        }
+
+        [TestMethod()]
+        public void TestNoWinner()
+        {
+            Result firstMatch = new Result(3, 0);
+            Result secondMatch = new Result(3, 0);
+            Match match = new Match("FTC", "Szeged", firstMatch, secondMatch);
+            string expected = "-";
+            string actaul = match.Winner;
+            Assert.AreEqual(expected, actaul, "Amikor nincs győztes nem jól hatorzta meg a kimenetet!");
+        }
+
+        [TestMethod()]
+        public void TestNoWinner2()
+        {
+            Result firstMatch = new Result(3, 3);
+            Result secondMatch = new Result(3, 3);
+            Match match = new Match("FTC", "Szeged", firstMatch, secondMatch);
+            string expected = "-";
+            string actaul = match.Winner;
+            Assert.AreEqual(expected, actaul, "Amikor nincs győztes nem jól hatorzta meg a kimenetet!");
+        }
+    }
+}
---
+++ tests/TestsResult.cs
@@ -0,0 +1,93 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+using MatchProject;
+
+namespace MatchProject.Tests
+{
+    [TestClass()]
+    public class TestsResult
+    {
+        [TestMethod()]
+        public void TestConstructor()
+        {
+            try
+            {
+                Result firstMatch = new Result(2, 1);
+            }
+            catch (Exception e)
+            {
+                Assert.Fail("A Result constructor hibát dob.");
+            }
+        }
+
+        [TestMethod()]
+        public void TestHomeTeamPoint()
+        {
+            Result match01 = new Result(2, 1);
+            Result match02 = new Result(1, 1);
+            Result match03 = new Result(1, 2);
+
+
+            int expected = 2;
+            int actaul = match01.HomeTeamPoint;
+            Assert.AreEqual(expected, actaul, "A hazai pontok számát nem jól határozta meg.");
+
+            expected = 1;
+            actaul = match02.HomeTeamPoint;
+            Assert.AreEqual(expected, actaul, "A hazai pontok számát nem jól határozta meg.");
+
+            expected = 0;
+            actaul = match03.HomeTeamPoint;
+            Assert.AreEqual(expected, actaul, "A hazai pontok számát nem jól határozta meg.");
+        }
+
+        [TestMethod()]
+        public void TestGuestTeamPointt()
+        {
+            Result match01 = new Result(2, 1);
+            Result match02 = new Result(1, 1);
+            Result match03 = new Result(1, 2);
+
+
+            int expected = 0;
+            int actaul = match01.GuestTeamPoint;
+            Assert.AreEqual(expected, actaul, "A vendég pontok számát nem jól határozta meg.");
+
+            expected = 1;
+            actaul = match02.GuestTeamPoint;
+            Assert.AreEqual(expected, actaul, "A vendég pontok számát nem jól határozta meg.");
+
+            expected = 2;
+            actaul = match03.GuestTeamPoint;
+            Assert.AreEqual(expected, actaul, "A vendég pontok számát nem jól határozta meg.");
+        }
+
+        [TestMethod()]
+        public void TestGoalDifference()
+        {
+            Result match01 = new Result(2, 1);
+            Result match02 = new Result(1, 1);
+            Result match03 = new Result(1, 2);
+
+
+            int expected = 1;
+            int actaul = match01.GoalDifference;
+            Assert.AreEqual(expected, actaul, "A gólkülöbséget nem jól határozta meg.");
+
+            expected = 0;
+            actaul = match02.GoalDifference;
+            Assert.AreEqual(expected, actaul, "A gólkülöbséget nem jól határozta meg.");
+
+            expected = -1;
+            actaul = match03.GoalDifference;
+            Assert.AreEqual(expected, actaul, "A gólkülöbséget nem jól határozta meg.");
+        }
+
+    }
+}
